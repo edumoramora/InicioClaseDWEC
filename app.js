aa864227af733efbf581694d2e62895d2afa6534
edumoramora <67929314+edumoramora@users.noreply.github.com>
2026-09-16 09:18:59 +0200

    Funcionalidad botón

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
+
+
+const boton = document.getElementById("miBoton");
+const parrafo = document.getElementById("presentacion");
+
+// 2. Creamos una variable para contar las pulsaciones
+let veces = 0;
+
+// 3. Le decimos al botón que "escuche" los clics.
+//    Cada vez que se pulse, se ejecutará esta función.
+boton.addEventListener("click", function () {
+  veces = veces + 1;   // sumamos uno al contador
+  parrafo.textContent = "Has pulsado el botón " + veces + " veces.";
+});
